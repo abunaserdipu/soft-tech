12802d396212304137ccabc95230c724568f0ef6
abunaserdipu <abunaserdipu0@gmail.com>
2021-11-11 07:40:54 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Admin
+
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin-auth/admin-auth.module').then(m => m.AdminAuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
